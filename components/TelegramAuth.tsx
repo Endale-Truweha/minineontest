5402afd23dbcfc58bab104a82d40701409d1854c
yonas <yonas2754@gmail.com>
2025-01-18 18:32:31 -0800

    fond problemcommit

--- components/TelegramAuth.tsx
+++ components/TelegramAuth.tsx
@@ -86,6 +86,7 @@ export default function TelegramAuth() {
 
                 <div className="fixed inset-0 flex items-center justify-center bg-ethLime-400 text-white">
                 <div className='   animate-spin'>
+                    <h1>{ JSON.stringify({ initData })}</h1>
                  <LoaderCircle size={100} strokeWidth={1.25} color="#6B5000" />
               
                  </div>
