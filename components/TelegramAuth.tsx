0944546a52925c4d3a27c9b923726e89211d37c4
yonas <yonas2754@gmail.com>
2025-01-18 18:47:32 -0800

    fond problemcommit

--- components/TelegramAuth.tsx
+++ components/TelegramAuth.tsx
@@ -87,7 +87,7 @@ export default function TelegramAuth() {
 
                 <div className="fixed inset-0 flex items-center justify-center bg-ethLime-400 text-white">
                 <div className='   animate-spin'>
-                    <h1>{isAuthenticated ? 'Authenticated' : 'Not authenticated'}</h1>
+                    <h1 className="text-4xl font-bold text-center text-black" >{isAuthenticated ? 'Authenticated' : 'Not authenticated'}</h1>
                  <LoaderCircle size={100} strokeWidth={1.25} color="#6B5000" />
               
                  </div>
